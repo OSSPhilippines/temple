114ff9357d56c2a520ebde55b7ee6b0be1384c0d
Chris Blanquera <chris@incept.asia>
2024-05-24 17:54:07 +0800

    with koa example

---
+++ examples/with-koa/src/index.ts
@@ -0,0 +1,38 @@
+import path from 'path';
+import Koa from 'koa';
+import Router from 'koa-router';
+import serve from 'koa-static';
+import { document } from '@ossph/temple/server';
+const app = new Koa();
+
+const template = document({
+  buildFolder: '../.temple',
+  cwd: __dirname,
+  useCache: false
+});
+
+const router = new Router();
+router.get('/', async (ctx, next) => {
+  const render = await template('./templates/page.tml');
+  ctx.body = render({
+    title: 'Temple',
+    description: 'Edit this file to change the content of the page.',
+    start: 0,
+    list: [
+      'Edit this file',
+      'Restyle this page',
+      'Create your own component',
+      'Star the Temple Repo',
+      'Write a blog post about Temple',
+      'Fork the respository',
+      'Contribute to the project'
+    ]
+  });
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());   
+app.use(serve(path.join(path.dirname(__dirname), 'public')));  
+
+
+app.listen(3000);
