66ff7384f960751d6622e83dc6f97424bf5c585f
Chris Blanquera <chris@incept.asia>
2024-08-28 19:28:11 +0800

    simplified implementation

--- packages/temple-express/src/index.ts
+++ packages/temple-express/src/index.ts
@@ -1,76 +1,55 @@
 import type { 
-  Hash,
   Request,
   Response,
-  TempleCompiler, 
-  BuildResults
+  TempleCompiler
 } from '@ossph/temple/compiler';
+
 import type { DevelopOptions } from '@ossph/temple-dev';
 
-import { inject, develop as dev } from '@ossph/temple-dev';
+import { dev as develop } from '@ossph/temple-dev';
 
-export type Next = (build: BuildResults, props: Hash) => string;
 export type NextView = (err: Error | null, results: string | undefined) => void;
 
-export const defaultNext: Next = (build, props) => build.document.render(props);
-
-export default function engine(
-  compiler: TempleCompiler, 
-  next: Next = defaultNext
-) {
+export function view(compiler: TempleCompiler) {
   return async (
     filePath: string,
     options: Record<string, any>,
-    callback: (err: Error | null, results: string | undefined) => void,
+    callback: NextView
   ) => {
     const { settings, _locals, cache, ...props } = options;
     try {
-      const build = await compiler.import(filePath);
-      callback(null, next(build, props));
+      callback(null, await compiler.render(filePath, props));
     } catch (e) {
+      //next
       callback(e as Error, undefined);
     }
   };
 };
 
-export function develop(
-  compiler: TempleCompiler, 
-  options: DevelopOptions = {}
-) {
-  const { refresh, serve } = dev(compiler, options);
+export function dev(options: DevelopOptions = {}) {
+  const { refresh, router } = develop(options);
 
   return {
-    compiler,
     refresh,
-    engine: async (
-      filePath: string,
-      options: Record<string, any>,
-      callback: NextView
-    ) => {
-      const { settings, _locals, cache, ...props } = options;
-      try {
-        //load the builder
-        const builder = compiler.builder(filePath);
-        //register the builder
-        refresh.register(builder, props);
-        //get the build object
-        const build = await builder.build();
-        //render the document
-        const html = inject(build.document.render(props));
-        callback(null, html);
-      } catch (e) {
-        callback(e as Error, undefined);
-      }
+    router: function(req: Request, res: Response, next: () => void) {
+      !router(req, res) && next();
     },
-    serve: function(req: Request, res: Response, next: () => void) {
-      !serve(req, res) && next();
+    view(compiler: TempleCompiler) {
+      return async (
+        filePath: string,
+        options: Record<string, any>,
+        callback: NextView
+      ) => {
+        const { settings, _locals, cache, ...props } = options;
+        try {
+          //sync builder with refresh server
+          refresh.sync(compiler.fromSource(filePath));
+          callback(null, await compiler.render(filePath, props));
+        } catch (e) {
+          //next
+          callback(e as Error, undefined);
+        }
+      }
     }
   };
-};
-
-export function build(compiler: TempleCompiler) {
-  const build = compiler.serve();
-  return function(req: Request, res: Response, next: () => void) {
-    build(req, res).then(resolved => !resolved && next());
-  }
 };
